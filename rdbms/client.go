d8057442029668a12248858ba511a614f096cdfb
Ben Halstead <ben.halstead@wolferton.com>
2016-09-22 10:40:27 +0100

    RDBMS development

--- rdbms/client.go
+++ rdbms/client.go
@@ -8,6 +8,23 @@ import (
 
 type InsertWithReturnedID func(string, *RDBMSClient, *int64) error
 
+func DefaultInsertWithReturnedID(query string, client *RDBMSClient, target *int64) error {
+
+	if r, err := client.Exec(query); err != nil {
+		return err
+	} else {
+		if id, err := r.LastInsertId(); err != nil {
+			return err
+		} else {
+
+			*target = id
+
+			return nil
+		}
+	}
+
+}
+
 func newRDBMSClient(database *sql.DB, querymanager querymanager.QueryManager, insertFunc InsertWithReturnedID) *RDBMSClient {
 	rc := new(RDBMSClient)
 	rc.db = database
