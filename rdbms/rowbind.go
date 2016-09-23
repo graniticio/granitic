1745d4445e7d0936314df2ac04241166d30f83b6
Ben Halstead <ben.halstead@wolferton.com>
2016-09-23 14:31:03 +0100

    RDBMS development

--- rdbms/rowbind.go
+++ rdbms/rowbind.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	rt "github.com/graniticio/granitic/reflecttools"
+	"github.com/graniticio/granitic/types"
 	"reflect"
 	"strconv"
 )
@@ -20,6 +21,7 @@ func (rb *RowBinder) BindRows(r *sql.Rows, t interface{}) ([]interface{}, error)
 
 	var err error
 	var columnNames []string
+	var targetScanners map[string]*scanner
 
 	if !rt.IsPointerToStruct(t) {
 		return nil, errors.New("Template must be a pointer to a struct.")
@@ -31,7 +33,10 @@ func (rb *RowBinder) BindRows(r *sql.Rows, t interface{}) ([]interface{}, error)
 
 	colCount := len(columnNames)
 
-	targetScanners := rb.generateTargets(t)
+	if targetScanners, err = rb.generateTargets(t); err != nil {
+		return nil, err
+	}
+
 	scanners := make([]interface{}, colCount)
 
 	results := make([]interface{}, 0)
@@ -82,7 +87,10 @@ func (rb *RowBinder) buildAndPopulate(t interface{}, scanners []interface{}) int
 		v := s.(*scanner)
 
 		f := rv.FieldByName(v.field)
-		f.Set(reflect.ValueOf(v.val))
+
+		if v.val != nil {
+			f.Set(reflect.ValueOf(v.val))
+		}
 
 	}
 
@@ -90,7 +98,7 @@ func (rb *RowBinder) buildAndPopulate(t interface{}, scanners []interface{}) int
 
 }
 
-func (rb *RowBinder) generateTargets(t interface{}) map[string]*scanner {
+func (rb *RowBinder) generateTargets(t interface{}) (map[string]*scanner, error) {
 
 	targets := make(map[string]*scanner)
 
@@ -109,6 +117,28 @@ func (rb *RowBinder) generateTargets(t interface{}) map[string]*scanner {
 			s.field = f.Name
 			s.kind = fv.Kind()
 
+			if s.kind == reflect.Ptr {
+
+				i := fv.Interface()
+
+				switch t := i.(type) {
+
+				case *types.NilableBool:
+					s.nilable = NilBool
+				case *types.NilableString:
+					s.nilable = NilString
+				case *types.NilableFloat64:
+					s.nilable = NilFloat
+				case *types.NilableInt64:
+					s.nilable = NilInt
+				default:
+					m := fmt.Sprintf("Unsupported type %T on target objects.", t)
+					return nil, errors.New(m)
+
+				}
+
+			}
+
 			if alias != "" {
 				targets[alias] = s
 			} else {
@@ -119,14 +149,24 @@ func (rb *RowBinder) generateTargets(t interface{}) map[string]*scanner {
 		}
 	}
 
-	return targets
+	return targets, nil
 }
 
-type scanner struct {
-	kind  reflect.Kind
-	field string
+type nilableType int
 
-	val interface{}
+const (
+	Unset = iota
+	NilBool
+	NilString
+	NilInt
+	NilFloat
+)
+
+type scanner struct {
+	kind    reflect.Kind
+	field   string
+	nilable nilableType
+	val     interface{}
 }
 
 func (s *scanner) Scan(src interface{}) error {
@@ -150,6 +190,8 @@ func (s *scanner) Scan(src interface{}) error {
 func (s *scanner) convert(sv string) error {
 
 	switch s.kind {
+	case reflect.Ptr:
+		return s.supportedStruct(sv)
 	case reflect.Int:
 		return s.toIntVal(sv, 0)
 	case reflect.Int8:
@@ -160,10 +202,84 @@ func (s *scanner) convert(sv string) error {
 		return s.toIntVal(sv, 32)
 	case reflect.Int64:
 		return s.toIntVal(sv, 64)
+	case reflect.Uint:
+		return s.toUintVal(sv, 0)
+	case reflect.Uint8:
+		return s.toUintVal(sv, 8)
+	case reflect.Uint16:
+		return s.toUintVal(sv, 16)
+	case reflect.Uint32:
+		return s.toUintVal(sv, 32)
+	case reflect.Uint64:
+		return s.toUintVal(sv, 64)
+	case reflect.Float32:
+		return s.toFloatVal(sv, 32)
+	case reflect.Float64:
+		return s.toFloatVal(sv, 64)
+	case reflect.Bool:
+		if b, err := strconv.ParseBool(sv); err != nil {
+			return err
+		} else {
+			s.val = b
+		}
 
 	default:
-		m := fmt.Sprintf("RowBinder: Unsupported type '%v' in target object.", s.kind)
+		m := fmt.Sprintf("RowBinder: Unsupported type '%v' in target object. Value from database was: %s", s.kind, sv)
 		return errors.New(m)
+
+	}
+
+	return nil
+}
+
+func (s *scanner) supportedStruct(sv string) error {
+
+	switch s.nilable {
+
+	case NilBool:
+
+		if b, err := strconv.ParseBool(sv); err != nil {
+			return err
+		} else {
+			s.val = types.NewNilableBool(b)
+		}
+
+	case NilInt:
+
+		if err := s.toIntVal(sv, 64); err != nil {
+			return err
+		} else {
+			s.val = types.NewNilableInt64(s.val.(int64))
+		}
+
+	case NilFloat:
+
+		if err := s.toFloatVal(sv, 64); err != nil {
+			return err
+		} else {
+			s.val = types.NewNilableFloat64(s.val.(float64))
+		}
+
+	case NilString:
+		s.val = types.NewNilableString(sv)
+
+	}
+
+	return nil
+
+}
+
+func (s *scanner) toFloatVal(sv string, size int) error {
+	if i, err := strconv.ParseFloat(sv, size); err != nil {
+		return err
+	} else {
+
+		switch size {
+		case 32:
+			s.val = float32(i)
+		case 64:
+			s.val = float64(i)
+		}
 
 	}
 
@@ -186,7 +302,31 @@ func (s *scanner) toIntVal(sv string, size int) error {
 		case 32:
 			s.val = int32(i)
 		case 64:
-			s.val = int16(i)
+			s.val = int64(i)
+		}
+
+	}
+
+	return nil
+}
+
+func (s *scanner) toUintVal(sv string, size int) error {
+
+	if i, err := strconv.ParseUint(sv, 10, size); err != nil {
+		return err
+	} else {
+
+		switch size {
+		case 0:
+			s.val = uint(i)
+		case 8:
+			s.val = uint8(i)
+		case 16:
+			s.val = uint16(i)
+		case 32:
+			s.val = uint32(i)
+		case 64:
+			s.val = uint16(i)
 		}
 
 	}
