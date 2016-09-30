4a5408aa6fa2d66ffdd39a41ce0b54a2e8ff6fe0
Ben Halstead <ben.halstead@wolferton.com>
2016-09-30 15:41:04 +0100

    GoDoc

--- ctl/handler.go
+++ ctl/handler.go
@@ -1,3 +1,6 @@
+// Copyright 2016 Granitic. All rights reserved.
+// Use of this source code is governed by an Apache 2.0 license that can be found in the LICENSE file at the root of this project.
+
 package ctl
 
 import (
