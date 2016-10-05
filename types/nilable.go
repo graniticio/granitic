b3cdfa2d66311c565a22a127e9afb47d5f0e83ed
Ben Halstead <ben.halstead@wolferton.com>
2016-10-05 14:29:21 +0100

    GoDoc

--- types/nilable.go
+++ types/nilable.go
@@ -1,5 +1,23 @@
+// Copyright 2016 Granitic. All rights reserved.
+// Use of this source code is governed by an Apache 2.0 license that can be found in the LICENSE file at the root of this project.
+
 /*
 Package types provides versions of Go's built-in types that better support web-services.
+
+Serialising data into and out of some of Go's native types can be problematic. This is because the zero value of numbers,
+bools and strings are not nil, but 0, false and "" respectively. This means the intent of the original data can be lost. Did the caller provider 'false' or just forget to specify a value?
+
+A similar problem is solved with Go's sql.NullXXX types for handling null values in and out of databases, but those types
+are not suitable for use with web services.
+
+Grantic defines a set of four 'nilable' types for handling int64, float64, bool and string values that might not always
+have a value associated with them. There is deep support for these types throughout Granitic including JSON and XML
+marhsalling/unmarshalling, path and query parameter binding, validation, query templating and RDBMS access. Developers
+are strongly encouraged to use nilable types instead of native types wherever possible.
+
+This package also defines a number of simple implmentations of a 'set'. Caution should be used when using these types in
+your own application as they are not goroutine safe or intended to store large numbers of strings.
+
 */
 package types
 
@@ -10,17 +28,58 @@ import (
 	"strconv"
 )
 
+// Implemented by a type that acts as a wrapper round a native type to track whether a value has actually been set.
 type Nilable interface {
+
+	// Convert the contained value to JSON or nil if no value is set.
 	MarshalJSON() ([]byte, error)
+
+	// Populate the type with the supplied JSON value or ignore if value is JSON null
 	UnmarshalJSON(b []byte) error
+
+	// Whether or not the value in this type was explicitly set
 	IsSet() bool
 }
 
+// Create a new NilableString with the supplied value.
+func NewNilableString(v string) *NilableString {
+	ns := new(NilableString)
+	ns.Set(v)
+
+	return ns
+}
+
+// Create a new NilableBool with the supplied value.
+func NewNilableBool(b bool) *NilableBool {
+	nb := new(NilableBool)
+	nb.Set(b)
+
+	return nb
+}
+
+// Create a new NilableInt64 with the supplied value.
+func NewNilableInt64(i int64) *NilableInt64 {
+	ni := new(NilableInt64)
+	ni.Set(i)
+
+	return ni
+}
+
+// Create a new NilableFloat64 with the supplied value.
+func NewNilableFloat64(f float64) *NilableFloat64 {
+	nf := new(NilableFloat64)
+	nf.Set(f)
+
+	return nf
+}
+
+// A string where it can be determined if "" is an explicitly set value, or just the default zero value
 type NilableString struct {
 	val string
 	set bool
 }
 
+// See Nilable.MarshalJSON
 func (ns *NilableString) MarshalJSON() ([]byte, error) {
 
 	if ns.set {
@@ -31,6 +90,7 @@ func (ns *NilableString) MarshalJSON() ([]byte, error) {
 
 }
 
+// See Nilable.UnmarshalJSON
 func (ns *NilableString) UnmarshalJSON(b []byte) error {
 	json.Unmarshal(b, &ns.val)
 	ns.set = true
@@ -38,31 +98,30 @@ func (ns *NilableString) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+// Set sets the contained value to the supplied value and makes IsSet true even if the supplied value is the empty
+// string.
 func (ns *NilableString) Set(v string) {
 	ns.val = v
 	ns.set = true
 }
 
+// The currently stored value (whether or not it has been explicitly set).
 func (ns *NilableString) String() string {
 	return ns.val
 }
 
+// See Nilable.IsSet
 func (ns *NilableString) IsSet() bool {
 	return ns.set
 }
 
-func NewNilableString(v string) *NilableString {
-	ns := new(NilableString)
-	ns.Set(v)
-
-	return ns
-}
-
+// A bool where it can be determined if false is an explicitly set value, or just the default zero value.
 type NilableBool struct {
 	val bool
 	set bool
 }
 
+// See Nilable.MarshalJSON
 func (nb *NilableBool) MarshalJSON() ([]byte, error) {
 
 	if nb.set {
@@ -73,6 +132,7 @@ func (nb *NilableBool) MarshalJSON() ([]byte, error) {
 
 }
 
+// See Nilable.UnmarshalJSON
 func (nb *NilableBool) UnmarshalJSON(b []byte) error {
 	s := string(b)
 
@@ -90,35 +150,34 @@ func (nb *NilableBool) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+// Set sets the contained value to the supplied value and makes IsSet true even if the supplied value is false.
 func (nb *NilableBool) Set(v bool) {
 	nb.val = v
 	nb.set = true
 }
 
+// See Nilable.IsSet
 func (nb *NilableBool) IsSet() bool {
 	return nb.set
 }
 
+// The currently stored value (whether or not it has been explicitly set).
 func (nb *NilableBool) Bool() bool {
 	return nb.val
 }
 
-func NewNilableBool(b bool) *NilableBool {
-	nb := new(NilableBool)
-	nb.Set(b)
-
-	return nb
-}
-
+// An int64 where it can be determined if 0 is an explicitly set value, or just the default zero value.
 type NilableInt64 struct {
 	val int64
 	set bool
 }
 
+// See Nilable.IsSet
 func (nb *NilableInt64) IsSet() bool {
 	return nb.set
 }
 
+// See Nilable.MarshalJSON
 func (ni *NilableInt64) MarshalJSON() ([]byte, error) {
 
 	if ni.set {
@@ -129,6 +188,7 @@ func (ni *NilableInt64) MarshalJSON() ([]byte, error) {
 
 }
 
+// See Nilable.UnmarshalJSON
 func (ni *NilableInt64) UnmarshalJSON(b []byte) error {
 	s := string(b)
 
@@ -145,31 +205,29 @@ func (ni *NilableInt64) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+// Set sets the contained value to the supplied value and makes IsSet true even if the supplied value is 0.
 func (ni *NilableInt64) Set(v int64) {
 	ni.val = v
 	ni.set = true
 }
 
+// The currently stored value (whether or not it has been explicitly set).
 func (ni *NilableInt64) Int64() int64 {
 	return ni.val
 }
 
-func NewNilableInt64(i int64) *NilableInt64 {
-	ni := new(NilableInt64)
-	ni.Set(i)
-
-	return ni
-}
-
+// An float64 where it can be determined if 0 is an explicitly set value, or just the default zero value.
 type NilableFloat64 struct {
 	val float64
 	set bool
 }
 
+// See Nilable.IsSet
 func (nb *NilableFloat64) IsSet() bool {
 	return nb.set
 }
 
+// See Nilable.MarshalJSON
 func (nf *NilableFloat64) MarshalJSON() ([]byte, error) {
 
 	if nf.set {
@@ -180,6 +238,7 @@ func (nf *NilableFloat64) MarshalJSON() ([]byte, error) {
 
 }
 
+// See Nilable.UnmarshalJSON
 func (nf *NilableFloat64) UnmarshalJSON(b []byte) error {
 	s := string(b)
 	v, err := strconv.ParseFloat(s, 64)
@@ -196,18 +255,13 @@ func (nf *NilableFloat64) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+// Set sets the contained value to the supplied value and makes IsSet true even if the supplied value is 0.
 func (nf *NilableFloat64) Set(v float64) {
 	nf.val = v
 	nf.set = true
 }
 
+// The currently stored value (whether or not it has been explicitly set).
 func (ni *NilableFloat64) Float64() float64 {
 	return ni.val
-}
-
-func NewNilableFloat64(f float64) *NilableFloat64 {
-	nf := new(NilableFloat64)
-	nf.Set(f)
-
-	return nf
 }
--- types/sets.go
+++ types/sets.go
@@ -1,47 +1,9 @@
+// Copyright 2016 Granitic. All rights reserved.
+// Use of this source code is governed by an Apache 2.0 license that can be found in the LICENSE file at the root of this project.
+
 package types
 
-type StringSet interface {
-	Contains(m string) bool
-	Add(s string)
-	Contents() []string
-	Size() int
-	AddAll(os StringSet)
-}
-
-type UnorderedStringSet struct {
-	members map[string]bool
-}
-
-func (us *UnorderedStringSet) AddAll(ss StringSet) {
-
-	for _, m := range ss.Contents() {
-		us.Add(m)
-	}
-}
-
-func (ss *UnorderedStringSet) Size() int {
-	return len(ss.members)
-}
-
-func (ss *UnorderedStringSet) Contents() []string {
-	c := make([]string, 0)
-
-	for k, _ := range ss.members {
-		c = append(c, k)
-	}
-
-	return c
-
-}
-
-func (ss *UnorderedStringSet) Contains(m string) bool {
-	return ss.members[m]
-}
-
-func (ss *UnorderedStringSet) Add(s string) {
-	ss.members[s] = true
-}
-
+// A new UnorderedStringSet seeded with the supplied strings.
 func NewUnorderedStringSet(m []string) *UnorderedStringSet {
 	ss := new(UnorderedStringSet)
 	ss.members = make(map[string]bool)
@@ -52,10 +14,12 @@ func NewUnorderedStringSet(m []string) *UnorderedStringSet {
 	return ss
 }
 
+// An empty OrderedStringSet
 func NewEmptyOrderedStringSet() *OrderedStringSet {
 	return NewOrderedStringSet([]string{})
 }
 
+// An OrderedStringSet with the supplied strings added to the new set in the provided order.
 func NewOrderedStringSet(m []string) *OrderedStringSet {
 	os := new(OrderedStringSet)
 
@@ -69,11 +33,76 @@ func NewOrderedStringSet(m []string) *OrderedStringSet {
 	return os
 }
 
+// Common behaviour for an ordered or unordered set of strings.
+type StringSet interface {
+	// Whether or not the set contains the supplied string.
+	Contains(m string) bool
+
+	// Add the supplied string to the set. If the set already contains the supplied value, it is ignored.
+	Add(s string)
+
+	// The members of the set a string slice.
+	Contents() []string
+
+	// The number of members of the set.
+	Size() int
+
+	// Add all the members of the supplied set to this set.
+	AddAll(os StringSet)
+}
+
+// A set of strings where the order in which the strings were added to the set is not recorded.
+//
+// This type is not goroutine safe and not recommended for the storage large number of strings.
+type UnorderedStringSet struct {
+	members map[string]bool
+}
+
+// See StringSet.AddAll
+func (us *UnorderedStringSet) AddAll(ss StringSet) {
+
+	for _, m := range ss.Contents() {
+		us.Add(m)
+	}
+}
+
+// See StringSet.Size
+func (ss *UnorderedStringSet) Size() int {
+	return len(ss.members)
+}
+
+// Contents returns all of the strings contained in this set in a nondeterministic order
+func (ss *UnorderedStringSet) Contents() []string {
+	c := make([]string, 0)
+
+	for k, _ := range ss.members {
+		c = append(c, k)
+	}
+
+	return c
+
+}
+
+// See StringSet.Contains
+func (ss *UnorderedStringSet) Contains(m string) bool {
+	return ss.members[m]
+}
+
+// See StringSet.Add
+func (ss *UnorderedStringSet) Add(s string) {
+	ss.members[s] = true
+}
+
+// A set of strings where the order in which the strings were added to the set is preserved. Calls to Contents will
+// return the strings in the same order in which they were added.
+
+// This type is not goroutine safe and not recommended for the storage of large number of strings.
 type OrderedStringSet struct {
 	members *UnorderedStringSet
 	ordered []string
 }
 
+// See StringSet.AddAll
 func (os *OrderedStringSet) AddAll(ss StringSet) {
 
 	for _, m := range ss.Contents() {
@@ -81,10 +110,12 @@ func (os *OrderedStringSet) AddAll(ss StringSet) {
 	}
 }
 
+// See StringSet.Contains
 func (os *OrderedStringSet) Contains(m string) bool {
 	return os.members.Contains(m)
 }
 
+// See StringSet.Add
 func (os *OrderedStringSet) Add(s string) {
 
 	if os.ordered == nil {
@@ -98,10 +129,12 @@ func (os *OrderedStringSet) Add(s string) {
 	os.members.Add(s)
 }
 
+// Contents returns the all of the strings in the set in the same order in which they were added.
 func (os *OrderedStringSet) Contents() []string {
 	return os.ordered
 }
 
+// See StringSet.Size
 func (os *OrderedStringSet) Size() int {
 	return len(os.ordered)
 }
