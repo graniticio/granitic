1de3d76fd044099ee45c20c605c47c1951fe091e
Ben Halstead <ben.halstead@brightpearl.com>
2016-10-10 19:00:33 +0100

    GoDoc

--- ioc/component.go
+++ ioc/component.go
@@ -1,5 +1,6 @@
 package ioc
 
+// What state (stopped, running) or state-tranistion a component is currently in.
 type ComponentState int
 
 const (
@@ -13,8 +14,14 @@ const (
 	ResumingState
 )
 
+// A wrapping structure for a list of ProtoComponents and FrameworkDependencies that is required when starting Granitic.
+// A ProtoComponents structure is built by the grnc-bind tool.
 type ProtoComponents struct {
-	Components            []*ProtoComponent
+	// ProtoComponents to be finalised and stored in the IoC container.
+	Components []*ProtoComponent
+
+	// FrameworkDependencies are instructions to inject components into built-in Granitic components to alter their behaviour.
+	// The structure is map[
 	FrameworkDependencies map[string]map[string]string
 }
 
@@ -22,6 +29,7 @@ func (pc *ProtoComponents) Clear() {
 	pc.Components = nil
 }
 
+// NewProtoComponents creates a wrapping structure for a list of ProtoComponents
 func NewProtoComponents(pc []*ProtoComponent, fd map[string]map[string]string) *ProtoComponents {
 	p := new(ProtoComponents)
 	p.Components = pc
@@ -29,9 +37,16 @@ func NewProtoComponents(pc []*ProtoComponent, fd map[string]map[string]string) *
 	return p
 }
 
+// A ProtoComponent is a partially configured component that will be hosted in the Granitic IoC container once
+// it is fully configured. Typically ProtoComponents are created using the grnc-bind tool.
 type ProtoComponent struct {
-	Component      *Component
-	Dependencies   map[string]string
+	// The name of a component and the component instance (a pointer to an instantiated struct).
+	Component *Component
+
+	// A map of fields on the component instance and the names of other components that should be injected into those fields.
+	Dependencies map[string]string
+
+	// A map of fields on the component instance and the config-path that will contain the configuration that shoud be inject into the field.
 	ConfigPromises map[string]string
 }
 
