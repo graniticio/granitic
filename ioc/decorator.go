0f913de6b85bd505cdafb7507dd6aed73b2532aa
Ben Halstead <ben.halstead@brightpearl.com>
2016-10-10 17:34:09 +0100

    GoDoc

--- ioc/decorator.go
+++ ioc/decorator.go
@@ -1,22 +1,38 @@
+// Copyright 2016 Granitic. All rights reserved.
+// Use of this source code is governed by an Apache 2.0 license that can be found in the LICENSE file at the root of this project.
+
 package ioc
 
+// ComponentDecorator is implemented by special temporary components that only exist while the IoC container is being populated.
+// A ComponentDecorator's job is to examine another component (the subject) to see if it is suitable for modification by the ComponentDecorator.
+// Typically this will involve the ComponentDecorator injecting another object into the subject if the component implements
+// a particular interface or has a writable field of a particular name or type.
+//
+// A number of built-in decorators exist to accomplish tasks like automatically adding Loggers to components with a particular field.
 type ComponentDecorator interface {
-	OfInterest(component *Component) bool
-	DecorateComponent(component *Component, container *ComponentContainer)
+	// OfInterest should return true if the ComponentDecorator decides that the supplied component needs to be decorated.
+	OfInterest(subject *Component) bool
+
+	// DecorateComponent modifies the subject component.
+	DecorateComponent(subject *Component, container *ComponentContainer)
 }
 
+// An interface implemented by any component that wants direct access to the IoC container.
 type ContainerAccessor interface {
+	// Container accepts a reference to the Granitic IoC container.
 	Container(container *ComponentContainer)
 }
 
+// ContainerDecorator injects a reference to the IoC container into any component implementing the ContainerAccessor interface.
 type ContainerDecorator struct {
 	container *ComponentContainer
 }
 
-func (cd *ContainerDecorator) OfInterest(component *Component) bool {
+// OfInterest returns true if the subject component implements ContainerAccessor
+func (cd *ContainerDecorator) OfInterest(subject *Component) bool {
 	result := false
 
-	switch component.Instance.(type) {
+	switch subject.Instance.(type) {
 	case ContainerAccessor:
 		result = true
 	}
@@ -24,9 +40,10 @@ func (cd *ContainerDecorator) OfInterest(component *Component) bool {
 	return result
 }
 
-func (cd *ContainerDecorator) DecorateComponent(component *Component, container *ComponentContainer) {
+// DecorateComponent injects a reference to the IoC container to a component that has alredy been determined to implement ContainerAccessor.
+func (cd *ContainerDecorator) DecorateComponent(subject *Component, cc *ComponentContainer) {
 
-	accessor := component.Instance.(ContainerAccessor)
-	accessor.Container(container)
+	accessor := subject.Instance.(ContainerAccessor)
+	accessor.Container(cc)
 
 }
