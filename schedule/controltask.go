05a377f5802a08e1b3bdbb02c455976fdc836033
Ben Halstead <benhalstead@gmail.com>
2018-07-25 11:18:30 +0100

    Start grnc-ctl support for tasks

---
+++ schedule/controltask.go
@@ -0,0 +1,24 @@
+package schedule
+
+import (
+	"github.com/graniticio/granitic/instance"
+	"github.com/graniticio/granitic/logging"
+)
+
+const (
+	LLComponentName = instance.FrameworkPrefix + "CommandScheduledTasks"
+	llCommandName   = "task"
+	llSummary       = "Shows information about all scheduled tasks or invokes/suspends a specified task"
+	llUsage         = "task [ID] [invoke|suspend|resume]"
+	llHelp          = "With no qualifier, this command shows a list of scheduled tasks defined for this service."
+	llHelpTwo   = "If a single qualifier is specified, that is assumed to be the ID of a task and more detailed information is shown for that task."
+	llHelpThree = "If a task ID is specified followed by invoke, that task is will be scheduled to run immediately (but task configuration is respected, so multiple conncurrent invocations might be forbidden."
+	llHelpFour  = "If a task ID is specified followed by suspend, that task will not be executed until resumed."
+	llHelpFive  = "If a task ID is specified followed by resumed, that task will be allowed to run again, if it is currently suspended"
+)
+
+type taskCommand struct {
+	FrameworkLogger    logging.Logger
+	FrameworkManager   *logging.ComponentLoggerManager
+	ApplicationManager *logging.ComponentLoggerManager
+}
