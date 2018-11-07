630a5cd3872436f803c35055affc900295c6d252
benhalstead <benhalstead@gmail.com>
2018-11-07 16:36:45 +0000

    IOC tests

---
+++ ioc/component_test.go
@@ -0,0 +1,34 @@
+package ioc
+
+import (
+	"github.com/graniticio/granitic/test"
+	"testing"
+)
+
+type dummyComp struct {
+}
+
+func TestProtoCreation(t *testing.T) {
+
+	pc := CreateProtoComponent(new(dummyComp), "TEST")
+
+	test.ExpectString(t, "TEST", pc.Component.Name)
+
+	if _, found := pc.Component.Instance.(*dummyComp); !found {
+		t.Errorf("Instance could not be converted to *dummyComp")
+		t.FailNow()
+	}
+
+}
+
+func TestAddDependencyAndPromises(t *testing.T) {
+
+	pc := CreateProtoComponent(new(dummyComp), "TEST")
+
+	pc.AddDependency("F1", "OTHER")
+	pc.AddConfigPromise("F2", "a.b.c")
+
+	test.ExpectString(t, pc.Dependencies["F1"], "OTHER")
+	test.ExpectString(t, pc.ConfigPromises["F2"], "a.b.c")
+
+}
