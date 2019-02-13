02cd7b002db8e9979ca2d5cc26e515942c7561ac
benhalstead <benhalstead@gmail.com>
2019-02-13 15:12:50 +0000

    File coverage in ws/json package

---
+++ ws/json/marshal_test.go
@@ -0,0 +1,55 @@
+package json
+
+import (
+	"context"
+	"github.com/graniticio/granitic/v2/ws"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestErrorMarshalling(t *testing.T) {
+
+	e := new(ws.ServiceErrors)
+	e.AddError(ws.NewCategorisedError(ws.Client, "AAA", "error"))
+
+	gef := new(GraniticJSONErrorFormatter)
+
+	f := gef.FormatErrors(e).(map[string]interface{})
+
+	g := f["General"]
+
+	if g == nil {
+		t.Fail()
+	}
+
+}
+
+func TestUnmarshalling(t *testing.T) {
+
+	r := new(http.Request)
+	sr := strings.NewReader("{\"A\": 1}")
+
+	rc := ioutil.NopCloser(sr)
+
+	r.Body = rc
+
+	um := new(Unmarshaller)
+
+	wsr := new(ws.Request)
+	wsr.RequestBody = new(target)
+
+	um.Unmarshall(context.Background(), r, wsr)
+
+	tar := wsr.RequestBody.(*target)
+
+	if tar.A != 1 {
+		t.Fail()
+	}
+
+}
+
+type target struct {
+	A int64
+}
