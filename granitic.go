2a362d79b48f87b555fb1de53fd6bf974476bb75
benhalstead <benhalstead@gmail.com>
2019-09-25 13:23:05 +0100

    Set 2.1.0 version

--- granitic.go
+++ granitic.go
@@ -80,7 +80,7 @@ import (
 
 const (
 	//Version is the semantic version number for this version of Granitic
-	Version                            = "2.0.3"
+	Version                            = "2.1.0"
 	initiatorComponentName      string = instance.FrameworkPrefix + "Init"
 	systemPath                         = "System"
 	configAccessorComponentName string = instance.FrameworkPrefix + "Accessor"
